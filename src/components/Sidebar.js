494ec497936011ac7b73e643daef360a9dcb6348
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-04-23 19:49:52 +0500

    sidebar instructed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,5 +2,31 @@ import React from "react";
 import { SidebarStyled } from "../styles/main";
 
 export default function Sidebar() {
-  return <SidebarStyled>SidebarStyled</SidebarStyled>;
+  return (
+    <SidebarStyled>
+      <div className="sidebar__top-block">
+        <div className="sidebar__head">input search</div>
+        <div className="sidebar__img">
+          <img srt="#" alt="weather-icon" />
+        </div>
+        <div className="sidebar__gradus">
+          <h3>12</h3>
+          <span className="sidebar__gradus-unit">C</span>
+        </div>
+        <h4 className="sidebar__date">
+          Monday, <span className="sidebar__time">15:00</span>
+        </h4>
+      </div>
+      <div className="sidebar__foot">
+        <div className="sidebar__row">
+          <div className="sidebar__icon">icon</div>
+          <div className="sidebar__label">label</div>
+        </div>
+        <div className="sidebar__row">
+          <div className="sidebar__icon">icon</div>
+          <div className="sidebar__label">label</div>
+        </div>
+      </div>
+    </SidebarStyled>
+  );
 }
