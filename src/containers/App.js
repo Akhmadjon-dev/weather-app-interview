4c4b9500ea80f769e9b81fb57b370f1bed1f1ac6
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-04-23 19:00:09 +0500

    removed

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
-import "../styles/app.css";
 import { Main, Section } from "../styles/main";
 
 function App() {
