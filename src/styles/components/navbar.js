869abd6e62dac546ea88fae4f850d1757a883c61
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-04-24 11:14:54 +0500

    main structure bug fixed

--- src/styles/components/navbar.js
+++ src/styles/components/navbar.js
@@ -2,4 +2,10 @@ import colors from "../colors";
 
 export const NavbarInner = `
   display: flex;
+  width: 100%;
+//   max-width: 1020px;
+//   background: aquamarine;
+  ul{
+      display:flex;
+  }
 `;
--- src/styles/main.js
+++ src/styles/main.js
@@ -9,6 +9,7 @@ export const Main = styled.div`
   justify-content: flex-start;
   height: 100vh;
   padding: 20px 30px;
+  flex-direction: column;
   background: ${colors.mainBc};
 `;
 export const SidebarStyled = styled.div`
@@ -16,7 +17,7 @@ export const SidebarStyled = styled.div`
   width: 290px;
   position: fixed;
   top: 20px;
-  bottom: 5px;
+  bottom: 20px;
   left: 30px;
   background: ${colors.white};
   border-top-left-radius: 30px;
@@ -27,29 +28,28 @@ export const SidebarStyled = styled.div`
   ${SidebarInner}
 `;
 export const NavbarStyled = styled.div`
-  position: fixed;
+  border-top-left-radius: 30px;
   display: flex;
   align-items: center;
   border-top-right-radius: 30px;
   width: 100%;
   height: 60px;
-  margin-left: 290px;
+  padding-left: 290px;
   padding-right: 30px;
   line-height: 60px;
   z-index: 100;
+  background: ${colors.bc};
   ${NavbarInner}
 `;
 
 export const Section = styled.section`
   width: 100%;
   height: 100%;
-
-  border-top-right-radius: 30px;
+  border-bottom-left-radius: 30px;
   border-bottom-right-radius: 30px;
   height: 96vh;
   padding: 15px;
-  margin-left: 290px;
-  padding-top: 60px;
+  padding-left: 290px;
   background: ${colors.bc};
   // @media (max-width: 1024px) {
   //   & {
